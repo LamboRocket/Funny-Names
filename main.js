19e574c6e73f1b8795cfc13e7ea0d6fa0002005d
LamboRocket <92550330+LamboRocket@users.noreply.github.com>
2022-05-17 18:44:25 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function enterName() {
   let nameInput = document.getElementById('name');
   let name = nameInput.value;
   let funnyName = document.getElementById('funny-name');
-  let adjectives = ['Creepy', 'Amazing', 'Jealous', 'Funny', 'Hairy', 'Gross', 'Bloody', 'Brave', 'Bored', 'Brainy', 'Bright', 'Careful']
+  let adjectives = ['Creepy', 'Amazing', 'Jealous', 'Funny', 'Hairy', 'Gross', 'Brave', 'Bored', 'Brainy', 'Bright', 'Careful']
   let randomItem = adjectives[Math.floor(Math.random() * adjectives.length)];
 
   funnyName.innerText = (name + ' is ' + randomItem);
